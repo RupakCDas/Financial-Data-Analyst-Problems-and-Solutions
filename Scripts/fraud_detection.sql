a8d02f4886374e79b8119d69c6a6a066c8b12d8a
Rupak Chandra Das <rupakcdp48@gmail.com>
2026-07-15 19:57:22 +0600

    Update and rename fraud_detect.sql to fraud_detection.sql

--- Scripts/fraud_detection.sql
+++ Scripts/fraud_detection.sql
@@ -78,4 +78,77 @@ anomalies AS (
 )
 SELECT * FROM anomalies ORDER BY amount DESC LIMIT 50;
 
+-- ── FRAUD RULE 3: Amount Z-score > 2 . Transaction is very large than avg. transaction ──
+-- Real-world: Single large unauthorized transaction after account compromise
+WITH stats AS (
+    SELECT
+        account_id,
+        AVG(amount)    AS avg_amount,
+        STDDEV(amount) AS std_amount
+    FROM transactions
+    GROUP BY account_id
+    HAVING COUNT(*) >= 5 AND STDDEV(amount) > 0
+)
+SELECT
+    t.txn_id,
+    t.account_id,
+    t.amount,
+    t.merchant,
+    t.txn_at,
+    ROUND((t.amount - s.avg_amount) / s.std_amount, 2) AS z_score,
+   ROUND( s.avg_amount, 2) AS avg_amount,
+   ROUND( s.std_amount, 2) AS std_dev
+FROM transactions t
+JOIN stats s ON t.account_id = s.account_id
+WHERE (t.amount - s.avg_amount) / s.std_amount > 2  -- Assuming stabdard deviation > 2 are suspicious
+ORDER BY z_score DESC;
+
+-- ── FRAUD RULE 4: Dormant account suddenly active ────────────────────────────
+-- Real-world: Stolen credentials — assuming account dormant 90+ days, then big purchase
+WITH last_activity AS (
+    SELECT
+        account_id,
+        txn_at,
+		LAG(txn_at) OVER (PARTITION BY account_id ORDER BY txn_at) AS prev_txn,
+        ROW_NUMBER() OVER( PARTITION BY account_id ORDER BY txn_at DESC) AS rn
+    FROM transactions
+    GROUP BY account_id, txn_at 
+  ),
+gaps AS(
+    SELECT
+        account_id,
+        txn_at AS last_txn,
+        prev_txn, 
+        DATEDIFF(
+			STR_TO_DATE(LEFT(txn_at, 19), '%Y-%m-%d %H:%i:%s') ,
+			STR_TO_DATE(LEFT(prev_txn, 19), '%Y-%m-%d %H:%i:%s')
+				) AS gap_days
+	FROM last_activity
+    WHERE rn = 1 AND prev_txn IS NOT NULL 
+	group by account_id, txn_at, prev_txn
+)
+SELECT account_id, 
+last_txn, 
+gap_days
+FROM gaps
+WHERE gap_days > 90 
+GROUP BY account_id, last_txn, gap_days
+ORDER BY gap_days DESC;
+
+-- ── FRAUD RULE 5: Round-number structuring (money laundering signal) ─────────
+-- Real-world: Assuming deposits just below $10K to avoid bank reporting threshold
+SELECT
+    account_id,
+    COUNT(*)          AS structured_txns,
+    SUM(amount)       AS total_amount,
+    MIN(txn_at)       AS first_seen,
+    MAX(txn_at)       AS last_seen
+FROM transactions
+WHERE amount BETWEEN 9000 AND 9999
+  AND txn_type IN ('deposit','transfer')
+GROUP BY account_id
+HAVING COUNT(*) >= 3
+ORDER BY total_amount DESC;
+
+
 
